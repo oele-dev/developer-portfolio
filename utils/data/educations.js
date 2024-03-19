07af43ca6541377b3a0f32fe50a07d7f02c9d544
Osmell Caicedo <correo.oele@gmail.com>
2024-03-19 12:59:30 -0500

    Update educations

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -4,11 +4,5 @@ export const educations = [
     title: "Associate's degree",
     duration: "2009 - 2011",
     institution: "Servicio Nacional de Aprendizaje (SENA)",
-  },
-  {
-    id: 2,
-    title: "Secondary School Certificate",
-    duration: "2004 - 2008",
-    institution: "Colegio Nuestra Señora de la Merced",
   }
 ]
